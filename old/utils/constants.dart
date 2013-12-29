ff626e572eb242c6c1627a7ce93c4e67af80ddb9
knarr <wayfaringknarr@gmail.com>
2013-12-29 17:53:26 -0500

    Deleted old folder and prepared for merge with master

--- old/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-// file: constants.dart
-library constants;
-//==============================================
-// Game Constants
-//==============================================
-const num g = 0.7; // gravitational acceleration
-const num mu = 0.95; // frictional coefficient
